e7f9b7fa254797e5adffe0c9246817deaea39488
Khyrous <khyrous@gmail.com>
2015-09-03 12:22:25 +1200

    testing autopush

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once("includes/header.php");
     <section><div class="mainText">
         <input type="checkbox" class="read-more-state" id="post-1"/>
         <p class="read-more-wrap">Welcome to KiwiBeats.co.nz<br>
-
+More text
 Top quality tuition by young relatable Kiwis.<br>
 Kiwi Beats has a passion to spread the love of music through enjoyable, stimulating and flexible lessons. <span class="read-more-target"> We steer away from the traditional spoon fed approach to music tuition and instead focus on inspiring all of our students to build a passion for music through exciting lesson plans and relatable young teachers. 
 <br>

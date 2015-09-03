86f4f74d7bf1a1045aaf0693c8ee1fbca579bca2
Khyrous <khyrous@gmail.com>
2015-09-03 13:33:44 +1200

    sticky footer

--- index.php
+++ index.php
@@ -5,14 +5,13 @@ require_once("includes/header.php");
     <section><div class="mainText">
         <input type="checkbox" class="read-more-state" id="post-1"/>
         <p class="read-more-wrap">Welcome to KiwiBeats.co.nz<br>
-More 
+
 Top quality tuition by young relatable Kiwis.<br>
 Kiwi Beats has a passion to spread the love of music through enjoyable, stimulating and flexible lessons. <span class="read-more-target"> We steer away from the traditional spoon fed approach to music tuition and instead focus on inspiring all of our students to build a passion for music through exciting lesson plans and relatable young teachers. 
 <br>
 We firmly believe that talent cannot thrive without passion. Our awesome team are flexible to your requirements and cannot wait to get started!  </span></p>
     <label for="post-1" class="read-more-trigger"></label>
     </div></section>
-
 </main>
 
 <?php
